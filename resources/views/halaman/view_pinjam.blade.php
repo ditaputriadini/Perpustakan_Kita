b0cc0678df56cefed14e61a4c2e6cccad354e453
Alda Desmalia Hafsari <aldadesmalia@gmail.com>
2023-07-11 13:30:18 +0700

    finalisasi alda bagian peminjaman

--- resources/views/halaman/view_pinjam.blade.php
+++ resources/views/halaman/view_pinjam.blade.php
@@ -3,7 +3,7 @@
 
 @section('isihalaman')
     <h3><center>Data Peminjaman Buku</center><h3>
-    <h3><center>Perpustakaan Kita</center></h3>
+    <h3><center>Perpustakaan Universitas Semarang</center></h3>
 
     <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalPinjamTambah">
         Tambah Data Peminjaman
